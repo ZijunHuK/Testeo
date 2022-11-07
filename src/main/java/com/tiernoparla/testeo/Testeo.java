283aa1c451030953c9be2a8311f7f132b5d7073a
Zijun Hu <zijun.hu@tiernoparla.es>
2022-11-07 21:50:49 +0100

    modify

--- src/main/java/com/tiernoparla/testeo/Testeo.java
+++ src/main/java/com/tiernoparla/testeo/Testeo.java
@@ -4,7 +4,7 @@ package com.tiernoparla.testeo;
 public class Testeo {
     
     public static void main(String[] args) {
-        System.out.println("Hello.Worl");
+        System.out.println("Hello.Worlgit");
     } //main
     
 } //Testeo
