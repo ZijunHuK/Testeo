3c4af3529c530373655e0bd82be2e68a87160f4c
Zijun Hu <zijun.hu@tiernoparla.es>
2022-11-07 21:44:32 +0100

    first commit

---
+++ src/main/java/com/tiernoparla/testeo/Testeo.java
@@ -0,0 +1,10 @@
+
+package com.tiernoparla.testeo;
+
+public class Testeo {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello.World");
+    } //main
+    
+} //Testeo
