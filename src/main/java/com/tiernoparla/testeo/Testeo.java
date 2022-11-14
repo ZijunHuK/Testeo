8ae803145a7daee9fb7bdccda2f7fd6d13079756
Zijun Hu <zijun.hu@tiernoparla.es>
2022-11-14 21:33:51 +0100

    add

--- src/main/java/com/tiernoparla/testeo/Testeo.java
+++ src/main/java/com/tiernoparla/testeo/Testeo.java
@@ -1,7 +1,7 @@
 
 package com.tiernoparla.testeo;
 
-public class TesteoCode {
+public class Testeo {
     
     public static final int EVEN_PARITY = 0;
     
@@ -15,9 +15,9 @@ public class TesteoCode {
     
     private Integer errorIndex;
     
-    public TesteoCode() {}
+    public Testeo() {}
     
-    public TesteoCode(int checkMode) {
+    public Testeo(int checkMode) {
         if (checkMode == ODD_PARITY) {
             this.checkMode = ODD_PARITY;
         } else {
